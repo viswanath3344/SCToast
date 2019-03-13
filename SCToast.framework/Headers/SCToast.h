9c89335baa331e2954296bfed665e83d019d004b
Viswa <Viswanath3344@gmail.com>
2019-03-13 10:52:11 +0530

    Initial commit

---
+++ SCToast.framework/Headers/SCToast.h
@@ -0,0 +1,19 @@
+//
+//  SCToast.h
+//  SCToast
+//
+//  Created by Ming-En Liu on 11/03/19.
+//  Copyright © 2019 Vedas labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCToast.
+FOUNDATION_EXPORT double SCToastVersionNumber;
+
+//! Project version string for SCToast.
+FOUNDATION_EXPORT const unsigned char SCToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCToast/PublicHeader.h>
+
+
